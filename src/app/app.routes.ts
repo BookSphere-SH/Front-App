4b2d82444d61d177c54cfaf459727ccdd343da13
Pedro Nanfuñay <pedro18nan@gmail.com>
2024-10-05 21:17:36 -0500

    Configuración de approutes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { StoreComponent } from "./public/pages/store/store.component";
+import { PublishBookComponent } from "./author/pages/publish-book/publish-book.component";
+import { LibraryComponent } from "./author/pages/library/library.component";
+import { PageNotFoundComponent } from "./public/pages/page-not-found/page-not-found.component";
+import { BookDetailsComponent } from "./author/components/book-details/book-details.component";
+
+export const routes: Routes = [
+  { path: 'store/store-books', component: StoreComponent },
+  { path: 'publish-book', component: PublishBookComponent },
+  { path: 'library', component: LibraryComponent},
+  { path: 'books/:id', component: BookDetailsComponent },
+  { path: '', redirectTo: 'store/store-books', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
