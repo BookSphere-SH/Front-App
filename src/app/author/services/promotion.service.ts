b3e7c335a27e093d3ea68294c3cd4f378e937070
Pedro Nanfuñay <pedro18nan@gmail.com>
2024-10-19 13:08:07 -0500

    Update promotion service.

--- src/app/author/services/promotion.service.ts
+++ src/app/author/services/promotion.service.ts
@@ -1,38 +1,54 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { PromotionItem } from "../model/promotion-item.entity";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PromotionService {
-  private apiUrl = 'http://localhost:3000/promotions'; // Ajustar a tu API
+  private promotions: PromotionItem[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  // Obtener una promoción específica
+  // Simular la obtención de una promoción
   getPromotion(id: number): Observable<PromotionItem> {
-    return this.http.get<PromotionItem>(`${this.apiUrl}/${id}`);
+    const promotion = this.promotions.find(promo => promo.id === id);
+    return of(promotion!); // Retorna el resultado como observable
   }
 
   // Activar una promoción
   activatePromotion(id: number): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}/activate`, {});
+    const promotion = this.promotions.find(promo => promo.id === id);
+    if (promotion) {
+      promotion.estado = 'Activa';
+    }
+    return of(promotion);
   }
 
   // Pausar una promoción
   pausePromotion(id: number): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}/pause`, {});
+    const promotion = this.promotions.find(promo => promo.id === id);
+    if (promotion) {
+      promotion.estado = 'Pausada';
+    }
+    return of(promotion);
   }
 
   // Reanudar una promoción
   resumePromotion(id: number): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}/resume`, {});
+    const promotion = this.promotions.find(promo => promo.id === id);
+    if (promotion) {
+      promotion.estado = 'Activa';
+    }
+    return of(promotion);
   }
 
   // Finalizar una promoción
   endPromotion(id: number): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}/end`, {});
+    const promotion = this.promotions.find(promo => promo.id === id);
+    if (promotion) {
+      promotion.estado = 'Finalizada';
+    }
+    return of(promotion);
   }
 }
