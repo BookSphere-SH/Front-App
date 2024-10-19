dbb01fae5fc73b2924e57b7016fd42c78b686b7f
Pedro Nanfuñay <pedro18nan@gmail.com>
2024-10-19 18:00:06 -0500

    Update view-details-published-book component.

--- src/app/author/components/view-details-published-book/view-details-published-book.component.ts
+++ src/app/author/components/view-details-published-book/view-details-published-book.component.ts
@@ -1,28 +1,30 @@
 import {Component, OnInit} from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { PublishedBooksService } from "../../services/published-books.service";
 import { PublishedBook } from "../../model/published-book.entity";
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from "@angular/material/button";
 import { PublishedBookCommentsComponent } from "../published-book-comments/published-book-comments.component";
 
 @Component({
   selector: 'app-view-details-published-book',
   standalone: true,
-  imports: [CommonModule, MatIconModule, PublishedBookCommentsComponent],
+  imports: [CommonModule, MatIconModule, MatButtonModule, RouterModule, PublishedBookCommentsComponent],
   templateUrl: './view-details-published-book.component.html',
   styleUrl: './view-details-published-book.component.css'
 })
 export class ViewDetailsPublishedBookComponent implements OnInit {
   publishedBook: PublishedBook | undefined;
 
-  constructor(private route: ActivatedRoute, private publishedBooksService: PublishedBooksService, private router: Router) {
+  constructor(private route: ActivatedRoute, private publishedBooksService: PublishedBooksService) {
   }
 
   ngOnInit(): void {
     this.getBookDetails();
   }
 
+  // Método para obtener los detalles del libro
   getBookDetails(): void {
     const publishedBookId = Number(this.route.snapshot.paramMap.get('id'));
     this.publishedBooksService.getPublishedBookById(publishedBookId).subscribe((response: PublishedBook) => {
